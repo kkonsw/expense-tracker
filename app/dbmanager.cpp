c78279dfaab46245bacead0ac4eaa60f865bcd2a
Kuznetsov Konstantin <kkonsw@gmail.com>
2020-11-17 20:46:53 +0300

    Add InitStorage as inline function

--- app/dbmanager.cpp
+++ app/dbmanager.cpp
@@ -1,14 +1,8 @@
 #include "dbmanager.h"
-#include "user.h"
 
-using namespace sqlite_orm;
 
 DBManager::DBManager()
 {
-    auto storage = make_storage("db.sqlite",
-                                make_table("users",
-                                           make_column("id", &User::id, autoincrement(), primary_key()),
-                                           make_column("first_name", &User::firstName),
-                                           make_column("last_name", &User::lastName)));
+    Storage storage = initStorage();
     storage.sync_schema();
 }
--- app/dbmanager.h
+++ app/dbmanager.h
@@ -2,6 +2,19 @@
 #define DBMANAGER_H
 
 #include "sqlite_orm.h"
+#include "user.h"
+
+using namespace sqlite_orm;
+
+inline auto initStorage(const std::string &path = "db.sqlite") {
+    return make_storage(path,
+                    make_table("users",
+                           make_column("id", &User::id, autoincrement(), primary_key()),
+                           make_column("first_name", &User::firstName),
+                           make_column("last_name", &User::lastName)));
+}
+
+using Storage = decltype(initStorage());
 
 class DBManager
 {
